7473c6a5a22bc1437b10905ec2dee747c1ba0086
roy.lieu <roy.lieu@gmail.com>
2019-10-10 09:55:51 +0000

    将算术和逻辑操作符归类到文件

--- runtime/arithmetic_operators.go
+++ runtime/arithmetic_operators.go
@@ -36,3 +36,35 @@ func Add(vm *VM) error {
 
 	return nil
 }
+
+func Mul(vm *VM) error {
+	// 从stack获取栈顶的2个元素
+	stackItemA := vm.Stack[vm.ESP-1]
+	stackItemB := vm.Stack[vm.ESP]
+
+	valueA := GetValueFromStack(vm, stackItemA)
+	valueB := GetValueFromStack(vm, stackItemB)
+
+	if err := CheckValue(valueA); err != nil {
+		return err
+	}
+
+	if err := CheckValue(valueB); err != nil {
+		return err
+	}
+
+	if valueA.Type == parser.VInt && valueB.Type == parser.VInt {
+		result := valueA.Value.(int64) * valueB.Value.(int64)
+		vm.ESP--
+		vm.Stack[vm.ESP] = &StackItem{
+			Type: STACK_TEMP,
+			Value: &Value{
+				Type:  parser.VInt,
+				Value: result,
+			},
+		}
+	}
+	utils.DebugPrintf("VM> MUL\n")
+
+	return nil
+}
--- runtime/logical_operators.go
+++ runtime/logical_operators.go
@@ -44,3 +44,44 @@ func Gt(vm *VM) error {
 	utils.DebugPrintf("VM> GT\n")
 	return nil
 }
+
+func Lt(vm *VM) error {
+	vm.ESP--
+	stackItemA := vm.Stack[vm.ESP]
+	stackItemB := vm.Stack[vm.ESP+1]
+
+	valueA := GetValueFromStack(vm, stackItemA)
+	valueB := GetValueFromStack(vm, stackItemB)
+
+	if err := CheckValue(valueA); err != nil {
+		return err
+	}
+	if err := CheckValue(valueB); err != nil {
+		return err
+	}
+
+	var stackItem *StackItem
+
+	if valueA.Value.(int64) < valueB.Value.(int64) {
+		stackItem = &StackItem{
+			Type: STACK_TEMP,
+			Value: &Value{
+				Type:  parser.VBool,
+				Value: true,
+			},
+		}
+	} else {
+		stackItem = &StackItem{
+			Type: STACK_TEMP,
+			Value: &Value{
+				Type:  parser.VBool,
+				Value: false,
+			},
+		}
+	}
+	vm.Stack[vm.ESP] = stackItem
+
+	utils.DebugPrintf("VM> LT\n")
+
+	return nil
+}
